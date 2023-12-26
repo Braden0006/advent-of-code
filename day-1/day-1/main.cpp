aad732cd0a9b3e4f15d8ef0745407536e68d4551
Braden0006 <bradenwhitch@gmail.com>
2023-12-26 16:44:14 -0700

    Added first bit of functionality in main.cpp

--- day-1/day-1/main.cpp
+++ day-1/day-1/main.cpp
@@ -1,11 +1,42 @@
 #include <iostream> 
 #include <string>
+#include <vector>
 
 using namespace std;
 
 int main() {
+	string inputValue;
+	vector<char> numbers;
 
-	cout << "Hello world" << endl;
+	char firstElement;
+	char secondElement;
+
+	string firstAndSecondElement;
+
+	int inputNumber;
+
+	int total = 0;
+
+	cout << "Enter in your values: ";
+
+	cin >> inputValue;
+
+	for (unsigned int i = 0; i < inputValue.size(); ++i) {
+		if (isdigit(inputValue[i])) {
+			numbers.push_back(inputValue[i]);
+		}
+	}
+
+	firstElement = numbers.front();
+	secondElement = numbers.back();
+
+	firstAndSecondElement = firstAndSecondElement + firstElement + secondElement;
+
+	inputNumber = stoi(firstAndSecondElement);
+
+	total += inputNumber;
+
+	cout << inputNumber;
 
 	return 0;
 }
